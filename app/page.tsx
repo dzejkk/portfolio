72c56c4aec289dadc91bcf246f68d72abfb64388
Jakub Kontra <kontrajakub2@gmail.com>
2025-01-06 14:16:54 +0000

    progress, projects section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Intro from '@/components/intro'
 import SectionDivider from '@/components/section-divider'
 import Image from 'next/image'
 import About from '@/components/about'
+import Projects from '@/components/projects'
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <Projects />
 
     </main>
   )
