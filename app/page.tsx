91177d1e078f9b6f41d8fb6fa48c7bc24ae3958c
Jakub Kontra <kontrajakub2@gmail.com>
2025-01-06 12:21:34 +0000

    added avatar + animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Intro from '@/components/intro'
 import Image from 'next/image'
+Intro
 
 export default function Home() {
   return (
     <main className="">
+      <Intro />
 
     </main>
   )
